ff0c765d411dd108e59cf2697357f545513a6ed9
Tyson Williams <tyson.j.williams@gmail.com>
2017-09-07 13:44:59 -0600

    Add Day 3 Challenge - Displaying Info from an Object. Add instructions

---
+++ seven-days-of-javascript/day3-display-object-info.js
@@ -0,0 +1,17 @@
+/*
+
+~~~ Task ~~~
+
+Write a JavaScript program to display the status (i.e. display book name, author name and reading status) of books.
+You are given an object library in the code's template. It contains a list of books with the above mentioned properties.
+Your task is to display the following:
+
+- If the book is unread:
+
+  You still need to read '<book_name>' by <author_name>.
+
+- If the book is read:
+
+  Already read '<book_name>' by <author_name>.
+
+*/
