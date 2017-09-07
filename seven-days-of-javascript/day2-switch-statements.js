49d3f5529430b2606f7b25c61617dd419b71d344
Tyson Williams <tyson.j.williams@gmail.com>
2017-09-07 12:45:00 -0600

    Add Day 2 Challenge: Switch Statements file. Setup coding challenge instructions

---
+++ seven-days-of-javascript/day2-switch-statements.js
@@ -0,0 +1,22 @@
+/*
+
+~~~ Task ~~~
+
+You are given a variable, . Your task is to print: 
+
+- ONE, if num is equal to . 
+- TWO, if num is equal to . 
+- THREE, if num is equal to .
+- FOUR, if num is equal to .
+- FIVE, if num is equal to .
+- SIX, if num is equal to .
+- SEVEN, if num is equal to .
+- EIGHT, if num is equal to .
+- NINE, if num is equal to .
+- PLEASE TRY AGAIN, if num is none of the above.
+
+~~~ Note ~~~
+
+Do not declare the variable num; it is declared inside our code checker. Use console.log to print statements to the console.
+
+*/
