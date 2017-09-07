c9fa4c57e68bc1a70ecb4a39b4f2154292957697
Tyson Williams <tyson.j.williams@gmail.com>
2017-09-07 12:51:17 -0600

    Add Day 2 Challenge: Objects. Setup coding challenge instructions

---
+++ seven-days-of-javascript/day2-objects.js
@@ -0,0 +1,19 @@
+/*
+
+~~~ Task ~~~
+
+In this example, you are given a single line consisting of property type values of a car in the following order:
+
+  TypeName ModelName ColorName
+
+These values are assigned to an object car that has the properties type, model and color (read the code in the editor carefully to learn how that is done). Your task is to complete the code to print the object.
+
+~~~ Sample Input ~~~
+
+  Fiat 500 White
+
+~~~ Sample Output ~~~
+
+  { type: 'Fiat', model: '500', color: 'White' }
+
+*/
