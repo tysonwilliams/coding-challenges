5fcd90f2b1e0f35ea2051cdc536cdb130727e0b1
Tyson Williams <tyson.j.williams@gmail.com>
2017-09-07 12:36:07 -0600

    Add Seven Days of JavaScript folder with first day challenge of if-else statements

---
+++ seven-days-of-javascript/day1-if-else-statements.js
@@ -0,0 +1,22 @@
+/*
+
+~~~ Task ~~~
+
+You are given a variable marks. Your task is to print: 
+
+- AA if marks is greater than . 
+- AB if marks is greater than  and less than or equal to . 
+- BB if marks is greater than  and less than or equal to . 
+- BC if marks is greater than  and less than or equal to . 
+- CC if marks is greater than  and less than or equal to . 
+- CD if marks is greater than  and less than or equal to . 
+- DD if marks is greater than  and less than or equal to . 
+- FF if marks is less than or equal to .
+
+~~~ Note ~~~
+
+Do not declare the variable marks. It is declared inside our code checker. 
+Use console.log for printing statements to the console.
+
+*/
+
