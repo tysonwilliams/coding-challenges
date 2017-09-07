7a085e0f155c010d863318dd54cf5444b7799d74
Tyson Williams <tyson.j.williams@gmail.com>
2017-09-07 12:46:43 -0600

    Update challenge instructions. Complete code challenge

--- seven-days-of-javascript/day2-switch-statements.js
+++ seven-days-of-javascript/day2-switch-statements.js
@@ -2,17 +2,17 @@
 
 ~~~ Task ~~~
 
-You are given a variable, . Your task is to print: 
+You are given a variable, num. Your task is to print: 
 
-- ONE, if num is equal to . 
-- TWO, if num is equal to . 
-- THREE, if num is equal to .
-- FOUR, if num is equal to .
-- FIVE, if num is equal to .
-- SIX, if num is equal to .
-- SEVEN, if num is equal to .
-- EIGHT, if num is equal to .
-- NINE, if num is equal to .
+- ONE, if num is equal to 1. 
+- TWO, if num is equal to 2. 
+- THREE, if num is equal to 3.
+- FOUR, if num is equal to 4.
+- FIVE, if num is equal to 5.
+- SIX, if num is equal to 6.
+- SEVEN, if num is equal to 7.
+- EIGHT, if num is equal to 8.
+- NINE, if num is equal to 9.
 - PLEASE TRY AGAIN, if num is none of the above.
 
 ~~~ Note ~~~
@@ -20,3 +20,35 @@ You are given a variable, . Your task is to print:
 Do not declare the variable num; it is declared inside our code checker. Use console.log to print statements to the console.
 
 */
+
+switch(num) {
+    case 1:
+        console.log("ONE");
+        break;
+    case 2:
+        console.log("TWO");
+        break;
+    case 3:
+        console.log("THREE");
+        break;
+    case 4:
+        console.log("FOUR");
+        break;
+    case 5:
+        console.log("FIVE");
+        break;
+    case 6:
+        console.log("SIX");
+        break;
+    case 7:
+        console.log("SEVEN");
+        break;
+    case 8:
+        console.log("EIGHT");
+        break;
+    case 9:
+        console.log("NINE");
+        break;
+    default:
+        console.log("PLEASE TRY AGAIN");
+}
