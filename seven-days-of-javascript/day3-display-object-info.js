1406f8391aa1e788ac55174c9ec062038e27740a
Tyson Williams <tyson.j.williams@gmail.com>
2017-09-07 13:46:05 -0600

    Setup given coding challenge environment

--- seven-days-of-javascript/day3-display-object-info.js
+++ seven-days-of-javascript/day3-display-object-info.js
@@ -15,3 +15,28 @@ Your task is to display the following:
   Already read '<book_name>' by <author_name>.
 
 */
+
+function displayInformation() {
+     // var library is defined, use it to print the information
+} 
+
+// tail starts here
+var library = [ 
+    {
+        title: 'Bill Gates',
+        author: 'The Road Ahead',
+        readingStatus: true
+    },
+    {
+        title: 'Steve Jobs',
+        author: 'Walter Isaacson',
+        readingStatus: true
+    },
+    {
+        title: 'Mockingjay: The Final Book of The Hunger Games',
+        author: 'Suzanne Collins',
+        readingStatus: false
+    }
+];
+
+displayInformation();
