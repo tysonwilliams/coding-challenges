50cfe6c460e0216b665a1d7033c688419bd01ac9
Tyson Williams <tyson.j.williams@gmail.com>
2017-09-07 14:16:52 -0600

    Add Day 3 Challenge - Sort Array of Objects. Setup given coding challenge environment

---
+++ seven-days-of-javascript/day3-sort-array-of-objects.js
@@ -0,0 +1,37 @@
+/*
+
+~~~ Task ~~~
+
+An array of objects, , is provided for you in the editor. Complete the code below so that it sorts 's elements alphabetically by book name and then prints the sorted  object.
+
+~~~ Note ~~~
+
+There is no input to be read, and there are no sample test cases.
+
+*/
+
+function sortLibrary() {
+    // var library is defined, use it in your code
+    // use console.log(library) to output the sorted library data
+} 
+
+// tail starts here
+var library = [
+    {
+        author: 'Bill Gates',
+        title: 'The Road Ahead',
+        libraryID: 1254
+    },
+    {
+        author: 'Steve Jobs',
+        title: 'Walter Isaacson',
+        libraryID: 4264
+    },
+    {
+        author: 'Suzanne Collins',
+        title: 'Mockingjay: The Final Book of The Hunger Games',
+        libraryID: 3245
+    }
+];
+
+sortLibrary();
