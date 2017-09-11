5cdc1378bf6d3a83c281731b021865ba2a260d8c
Tyson Williams <tyson.j.williams@gmail.com>
2017-09-11 15:08:48 -0600

    Complete Fetch and Show First Planet challenge

--- promises/exoplanet-explorer/app/scripts/app.js
+++ promises/exoplanet-explorer/app/scripts/app.js
@@ -1,9 +1,10 @@
 /*
 Instructions:
-(1) Wrap an XHR in a Promise in the get() function below. See: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
-  (a) Resolve on load and reject on error.
-(2) If the XHR resolves, use addSearchHeader to add the search header to the page.
-(3) If the XHR fails, console.log the error and pass 'unknown' to addSearchHeader
+(1) Get the planet data and add the search header.
+(2) Create the first thumbnail with createPlanetThumb(data)
+(3) Handle errors!
+  (a) Pass 'unknown' to the search header.
+  (b) console.log the error.
  */
 
 // Inline configuration for jshint below. Prevents `gulp jshint` from failing with quiz starter code.
@@ -16,26 +17,40 @@ Instructions:
 
   /**
    * Helper function to show the search query.
-   * @param {String} response - The unparsed JSON response from get.
+   * @param {String} query - The search query.
    */
-  function addSearchHeader(response) {
-    try {
-      response = JSON.parse(response).query;  // you'll be moving this line out of here in the next quiz!
-    } catch (e) {
-      // it's 'unknown', so leave it alone
-    }
-    home.innerHTML = '<h2 class="page-title">query: ' + response + '</h2>';
+  function addSearchHeader(query) {
+    home.innerHTML = '<h2 class="page-title">query: ' + query + '</h2>';
   }
 
   /**
-   * XHR wrapped in a promise.
+   * Helper function to create a planet thumbnail.
+   * @param  {Object} data - The raw data describing the planet.
+   */
+  function createPlanetThumb(data) {
+    var pT = document.createElement('planet-thumb');
+    for (var d in data) {
+      pT[d] = data[d];
+    }
+    home.appendChild(pT);
+  }
+
+  /**
+   * XHR wrapped in a promise
    * @param  {String} url - The URL to fetch.
    * @return {Promise}    - A Promise that resolves when the XHR succeeds and fails otherwise.
    */
   function get(url) {
-    return fetch(url);
+    return fetch(url, {
+      method: 'get'
+    });
   }
 
+  /**
+   * Performs an XHR for a JSON and returns a parsed JSON response.
+   * @param  {String} url - The JSON URL to fetch.
+   * @return {Promise}    - A promise that passes the parsed JSON response.
+   */
   function getJSON(url) {
     return get(url).then(function(response) {
       return response.json();
@@ -44,19 +59,21 @@ Instructions:
 
   window.addEventListener('WebComponentsReady', function() {
     home = document.querySelector('section[data-route="home"]');
-    /*
-    Uncomment the next line you're ready to start chaining and testing!
-    You'll need to add a .then and a .catch. Pass the response to addSearchHeader on resolve or
-    pass 'unknown' to addSearchHeader if it rejects.
-     */
-    get('http://udacity.github.io/exoplanet-explorer/site/app/data/earth-like-results.json')
+
+    getJSON('../data/earth-like-results.json')
     .then(function(response) {
       addSearchHeader(response.query);
-      console.log(response);
+      return getJSON(response.results[0]);
     })
-    .catch(function(error) {
-      addSearchHeader('unknown');
-      console.log(error);
-    });
+    .catch(function() {
+      throw Error ("Search Request Error");
+    })
+    .then(function(planetData) {
+      createPlanetThumb(planetData);
+    })
+    .catch(function(e) {
+      addSearchHeader("unknown");
+      console.log(e);
+    })
   });
 })(document);
