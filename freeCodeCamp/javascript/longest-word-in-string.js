0442ff291c0314558f30be5aae998140743b3ae3
Tyson Williams <tyson.j.williams@gmail.com>
2017-09-21 11:12:15 -0600

    Complete challenge

--- freeCodeCamp/javascript/longest-word-in-string.js
+++ freeCodeCamp/javascript/longest-word-in-string.js
@@ -1,5 +1,12 @@
 function findLongestWord(str) {
-  return str.length;
+  // Split string into an array of separate words
+  const wordArray = str.split(" ");
+  
+  // Sort words by length from smallest to largest
+  const orderedWordArray = wordArray.sort((word1, word2) => word1.length > word2.length);
+  
+  // Return largest word's length, which is last word in newly sorted array
+  return orderedWordArray.pop().length;
 }
 
 findLongestWord("The quick brown fox jumped over the lazy dog");
