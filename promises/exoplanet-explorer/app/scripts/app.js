7565d0d0c42c8b1c256921b89be632344f564651
Tyson Williams <tyson.j.williams@gmail.com>
2017-09-21 11:08:04 -0600

    Add challenge folder for freeCodeCamp JavaScript challenges. Update exoplanet challenge

--- promises/exoplanet-explorer/app/scripts/app.js
+++ promises/exoplanet-explorer/app/scripts/app.js
@@ -1,10 +1,8 @@
 /*
-Instructions:
-(1) Get the planet data and add the search header.
-(2) Create the first thumbnail with createPlanetThumb(data)
-(3) Handle errors!
-  (a) Pass 'unknown' to the search header.
-  (b) console.log the error.
+Challenge:
+
+Use what you've learned about Promises to request thumbnails in parallel but create them in the
+proper order even if all the requests haven't finished.
  */
 
 // Inline configuration for jshint below. Prevents `gulp jshint` from failing with quiz starter code.
@@ -24,15 +22,19 @@ Instructions:
   }
 
   /**
-   * Helper function to create a planet thumbnail.
+   * Helper function to create a planet thumbnail - Promisified version!
    * @param  {Object} data - The raw data describing the planet.
    */
   function createPlanetThumb(data) {
-    var pT = document.createElement('planet-thumb');
-    for (var d in data) {
-      pT[d] = data[d];
-    }
-    home.appendChild(pT);
+    return new Promise(function(resolve) {
+      var pT = document.createElement('planet-thumb');
+      for (var d in data) {
+        pT[d] = data[d];
+      }
+      home.appendChild(pT);
+      console.log('rendered: ' + data.pl_name);
+      resolve();
+    });
   }
 
   /**
@@ -41,19 +43,29 @@ Instructions:
    * @return {Promise}    - A Promise that resolves when the XHR succeeds and fails otherwise.
    */
   function get(url) {
-    return fetch(url, {
-      method: 'get'
-    });
+    return fetch(url);
   }
 
   /**
-   * Performs an XHR for a JSON and returns a parsed JSON response.
+   * Performs an XHR for a JSON and returns a parsed JSON response - with a delay!
    * @param  {String} url - The JSON URL to fetch.
    * @return {Promise}    - A promise that passes the parsed JSON response.
    */
   function getJSON(url) {
+    console.log('sent: ' + url);
     return get(url).then(function(response) {
-      return response.json();
+      // For testing purposes, I'm making sure that the urls don't return in order
+      if (url === 'data/planets/Kepler-62f.json') {
+        return new Promise(function(resolve) {
+          setTimeout(function() {
+            console.log('received: ' + url);
+            resolve(response.json());
+          }, 500);
+        });
+      } else {
+        console.log('received: ' + url);
+        return response.json();
+      }
     });
   }
 
@@ -63,17 +75,26 @@ Instructions:
     getJSON('../data/earth-like-results.json')
     .then(function(response) {
       addSearchHeader(response.query);
-      return getJSON(response.results[0]);
+      return response;
     })
-    .catch(function() {
-      throw Error ("Search Request Error");
-    })
-    .then(function(planetData) {
-      createPlanetThumb(planetData);
-    })
-    .catch(function(e) {
-      addSearchHeader("unknown");
-      console.log(e);
-    })
+    .then(function(response) {
+      var sequence = Promise.resolve();
+
+      // .map executes all of the network requests immediately.
+      var arrayOfExecutingPromises = response.results.map(function(result) {
+        return getJSON(result);
+      });
+
+      arrayOfExecutingPromises.forEach(function (request) {
+        // Loop through the pending requests that were returned by .map (and are in order) and
+        // turn them into a sequence.
+        // request is a getJSON() that's currently executing.
+        sequence = sequence.then(function() {
+          // Remember that createPlanetThumb is a Promise, so it must resolve before Promises
+          // later in the sequence can execute.
+          return request.then(createPlanetThumb);
+        });
+      });
+    });
   });
 })(document);
