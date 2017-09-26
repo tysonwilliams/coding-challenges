8777d3ad13965c0299f7112865ac27f055165939
Tyson Williams <tyson.j.williams@gmail.com>
2017-09-26 12:19:31 -0600

    Complete challange and add in test array

--- seven-days-of-javascript/day4-arrows.js
+++ seven-days-of-javascript/day4-arrows.js
@@ -14,5 +14,10 @@ The name of the array parameter can be anything. For example, some_array.
 
 */
 
+// test with a numbered array
+var testArray = [1, 2, 3];
+
 // write the correct arrow function here
-var my_function = ______________________________________________________;
+var my_function = numberArray => numberArray.map(number => (number % 2 === 0) ? number + 1 : number - 1);
+
+console.log(my_function(testArray)); // [0, 3, 2]
