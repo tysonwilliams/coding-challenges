b5d335360f80e56a93e40081e73f3db38aa553b3
Tyson Williams <tyson.j.williams@gmail.com>
2017-09-26 12:15:18 -0600

    Setup challenge environment for ES6 Arrow syntax

---
+++ seven-days-of-javascript/day4-arrows.js
@@ -0,0 +1,18 @@
+/*
+
+~~~ Task ~~~
+
+You are given a variable, my_function. Your task is to assign it with an arrow function. 
+The my_function should take an array as its parameter and return an array with all its even elements incremented by , and odd elements decremented by .
+
+~~~ Note ~~~
+
+DON'T use function instead of an arrow function.
+DON'T print anything on the console.
+Replace the blank (_________) with an arrow function.
+The name of the array parameter can be anything. For example, some_array.
+
+*/
+
+// write the correct arrow function here
+var my_function = ______________________________________________________;
