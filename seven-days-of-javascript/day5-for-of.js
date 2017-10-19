87d76d769d45046d1d119266bc0254d8e17adabf
Tyson Williams <tyson.j.williams@gmail.com>
2017-10-19 10:53:07 -0600

    Setup up challenge

---
+++ seven-days-of-javascript/day5-for-of.js
@@ -0,0 +1,12 @@
+/*
+
+~~~ Task ~~~
+
+We have created an array my_array whose elements are strings. Your task is to print the elements which are palindromes.
+
+~~~ Note ~~~
+
+Do not declare the variable my_array.
+Add 'use strict'; at the top of your solution to use let in the for...of loop.
+
+*/
